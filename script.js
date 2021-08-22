03bc6647f35b3215c98987ccdfbc492ce467aa9e
Ian <lsouza.ian@gmail.com>
2021-08-22 14:57:22 -0300

    another fix for the image

--- script.js
+++ script.js
@@ -9,4 +9,17 @@ const getActivity = async () => {
   }
 }
 
-getActivity();
+const getImage = async () => {
+  try {
+    await axios.get('https://picsum.photos/600/400').then((res) => {
+      document.getElementById('activity-picture').style.backgroundImage = `url('${res.config.url}')`;
+    })
+  } catch(err) {
+    console.error(err)
+  }
+}
+
+(function data(){
+  getActivity();
+  getImage();
+})();
