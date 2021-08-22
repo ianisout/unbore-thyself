37d8ee166e68ff8e5db3c6d3d52181d319e47612
Ian <lsouza.ian@gmail.com>
2021-08-22 11:18:19 -0300

    try fix img load

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const getActivity = async () => {
     await axios.get('http://www.boredapi.com/api/activity/').then((res) => {
       let data = res.data;
       document.getElementById('activity-data').innerHTML = `${data.activity}!`;
-      document.getElementById('activity-img').src = `https://picsum.photos/600/400?random=${Math.floor(Math.random() *1000)}`
     })
   } catch(err) {
     console.error(err);
   }
+  document.getElementById('activity-img').src = `https://picsum.photos/600/400?random=${Math.floor(Math.random() *1000)}`
 }
 
 getActivity();
