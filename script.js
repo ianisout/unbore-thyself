89b0f1d3e22cb900a5cec08dc5c1edf80eea9eca
Ian <lsouza.ian@gmail.com>
2021-08-22 15:04:28 -0300

    fix bored API 2

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const getActivity = async () => {
   try {
-    await axios.get('http://www.boredapi.com/api/activity/').then((res) => {
+    await axios.get('https://www.boredapi.com/api/activity/').then((res) => {
       let data = res.data;
       document.getElementById('activity-data').innerHTML = `${data.activity}!`;
-      console.log(res.data.activity)
     })
   } catch(err) {
     console.error(err);
