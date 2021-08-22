d70b0e3286a47e339e5e6ba5219ac320ba37cb2e
Ian <lsouza.ian@gmail.com>
2021-08-22 15:08:16 -0300

    test fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const getActivity = async () => {
   try {
-    await axios.get('https://www.boredapi.com/api/activity/').then((res) => {
+    await axios.get('http://www.boredapi.com/api/activity/').then((res) => {
       let data = res.data;
       document.getElementById('activity-data').innerHTML = `${data.activity}!`;
     })
