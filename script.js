5193ae48e39e5748d65b916f013d4e248389bb07
Ian <lsouza.ian@gmail.com>
2021-08-22 15:17:59 -0300

    final cleanup, it was working all along

--- script.js
+++ script.js
@@ -9,4 +9,17 @@ const getActivity = async () => {
   }
 };
 
+const getImage = async () => {
+  try {
+    await axios.get('https://picsum.photos/600/400').then((res) => {
+      document.getElementById(
+        'activity-picture'
+      ).style.backgroundImage = `url('${res.config.url}')`;
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 getActivity();
+getImage();
