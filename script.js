2a12f160617520a9ba7cca22048ca6de758ce26e
Ian <lsouza.ian@gmail.com>
2021-08-22 11:20:59 -0300

    try fix img load 2

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+const getImage = () => document.getElementById('activity-img').src = `https://picsum.photos/600/400?random=${Math.floor(Math.random() *1000)}`
 
 const getActivity = async () => {
   try {
     await axios.get('http://www.boredapi.com/api/activity/').then((res) => {
       let data = res.data;
       document.getElementById('activity-data').innerHTML = `${data.activity}!`;
+      getImage();
     })
   } catch(err) {
     console.error(err);
   }
-  document.getElementById('activity-img').src = `https://picsum.photos/600/400?random=${Math.floor(Math.random() *1000)}`
 }
 
 getActivity();
