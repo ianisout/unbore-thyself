ed3feaef033773b34cd4adbee19619845bc65f41
Ian LS <iansouza.sjc@gmail.com>
2022-12-31 06:52:27 -0300

    fix cors

--- script.js
+++ script.js
@@ -9,19 +9,19 @@ const getActivity = async () => {
   }
 };
 
-// const getImage = async () => {
-//   try {
-//     await axios.get('https://picsum.photos/600/400').then((res) => {
-//       document.getElementById(
-//         'activity-picture'
-//       ).style.backgroundImage = `url('${res.config.url}')`;
-//     });
-//   } catch (err) {
-//     console.error(err);
-//   }
-// };
+const getImage = async () => {
+  try {
+    await axios.get('https://picsum.photos/600/400').then((res) => {
+      document.getElementById(
+        'activity-picture'
+      ).style.backgroundImage = `url('${res.config.url}')`;
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 
 
 getActivity();
-// getImage();
+getImage();
